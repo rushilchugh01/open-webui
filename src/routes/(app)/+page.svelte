bf1ab4555734c5f7c5ed19068ef41dd0dcecef6f
Rushil Chugh <rushilchugh01@gmail.com>
2024-12-29 21:34:19 +0530

    Implemented splitpanes to divide workspace

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import Chat from '$lib/components/chat/Chat.svelte';
 	import Help from '$lib/components/layout/Help.svelte';
+	import Workdeck from '$lib/components/layout/Workdeck.svelte'
 </script>
 
 <Help />
-<Chat />
+<Workdeck />
